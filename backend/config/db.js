e5425d1a3b6d1d7b0786f6d589fc5fe65a99d281
Vinoth-404 <bvinoth76@gmail.com>
2026-06-25 07:27:35 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("DB Connected Successfully");
+    // Run DB Migrations
+    const runMigration = require("./migrate");
+    runMigration();
+  }
+});
+
+module.exports = connection;
