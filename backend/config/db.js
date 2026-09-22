7c0a46ba3551e083a94c75e99e21b6605cd11499
Vinoth B <bvinoth76@gmail.com>
2026-09-22 11:48:26 +0530

    Configure test database SSL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,23 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
+const fs = require("fs");
+
+const getSslOption = () => {
+    if (process.env.DB_SSL !== "true" && process.env.DB_SSL !== "1") {
+        return undefined;
+    }
+    let ca = process.env.DB_SSL_CA;
+    if (ca) {
+        try {
+            if (fs.existsSync(ca)) {
+                ca = fs.readFileSync(ca, "utf8");
+            }
+        } catch (err) {
+            // ca is certificate content string
+        }
+    }
+    return ca ? { ca, rejectUnauthorized: true } : { rejectUnauthorized: true };
+};
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -11,7 +29,8 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
     enableKeepAlive: true,
-    keepAliveInitialDelay: 0
+    keepAliveInitialDelay: 0,
+    ssl: getSslOption()
 });
 
 module.exports = pool;
